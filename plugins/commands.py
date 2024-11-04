60a2a38708bd627ce5ac57be591b5cda8a41341e
Lectral <120367292+Lec7ral@users.noreply.github.com>
2024-11-04 11:43:48 -0500

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -9,7 +9,7 @@ from translation import Translation
 from pyrogram import Client, filters, enums, __version__ as pyrogram_version
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaDocument, BotCommand
 from .settings import settings_query
-from .userSettings import user_settings_query 
+from .userSettings import user_settings_query   
 main_buttons = [[
         InlineKeyboardButton('❗ ʜᴇʟᴘ', callback_data='help')],[
         InlineKeyboardButton('🎓 ᴀʙᴏᴜᴛ ', callback_data='about')
